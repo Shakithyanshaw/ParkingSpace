697573e8a1fbf5ab10db7f280b9744668e7ea37a
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2025-05-09 16:33:43 +0530

    grayscale and Gaussian blur preprocessing

--- main.py
+++ main.py
@@ -10,6 +10,13 @@ with open('CarParkPos', 'rb') as f:
     posList = pickle.load(f)   
 width, height = 107, 48
 
+def checkParkingSpace():
+    for pos in posList:
+        x,y = pos
+
+        imgCrop = img[y:y+height, x:x+width]
+        cv2.imshow(str(x*y), imgCrop)
+
 
 while True:
 
@@ -17,10 +24,15 @@ while True:
         cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
     success, img = cap.read()
-    
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray, (3, 3), 1)
+
+
+    checkParkingSpace()
     for pos in posList:
         cv2.rectangle(img, pos, (pos[0]+width,pos[1]+height), (255, 0, 255), 2)
 
 
     cv2.imshow("image", img)
+    cv2.imshow("imageBlur", imgBlur)
     key = cv2.waitKey(10)
