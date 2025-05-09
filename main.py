7b1e4d1e2d9d7d195e375585ba3b7776bed3dd0b
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2025-05-09 15:47:30 +0530

    Open the video file in main

---
+++ main.py
@@ -0,0 +1,16 @@
+import cv2
+import pickle
+import cvzone
+import numpy as np
+
+#video-feed
+cap = cv2.VideoCapture('carPark.mp4')
+
+while True:
+
+    if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+
+    success, img = cap.read()
+    cv2.imshow("image", img)
+    key = cv2.waitKey(10)
